c03714a8bffc292b6d8b24dbbf001825fbc27522
guyo-halake <guyohalakeofficial@gmail.com>
2026-04-22 16:27:22 +0300

    razak: synced src/scratch/check_school_fees.cjs

---
+++ src/scratch/check_school_fees.cjs
@@ -0,0 +1,26 @@
+const https = require('https');
+
+const options = {
+  hostname: 'vomsaqkhtturzqfuwxsn.supabase.co',
+  port: 443,
+  path: '/rest/v1/fees?select=type,amount_due,amount_paid,status&school_id=eq.f01e2ad5-9dbe-4df8-bb23-0d9e56f113f5&limit=10',
+  method: 'GET',
+  headers: {
+    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbXNhcWtodHR1cnpxZnV3eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MDIxNjgsImV4cCI6MjA5MTI3ODE2OH0.3M2p0SWKO51yjkGtLnCyJOFsKiAwC__BnWoe_jZlPI8',
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbXNhcWtodHR1cnpxZnV3eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MDIxNjgsImV4cCI6MjA5MTI3ODE2OH0.3M2p0SWKO51yjkGtLnCyJOFsKiAwC__BnWoe_jZlPI8'
+  }
+};
+
+const req = https.request(options, (res) => {
+  let data = '';
+  res.on('data', (chunk) => { data += chunk; });
+  res.on('end', () => {
+    console.log('--- SCHOOL FEES DATA ---');
+    console.log(data);
+  });
+});
+
+req.on('error', (e) => {
+  console.error(e);
+});
+req.end();
