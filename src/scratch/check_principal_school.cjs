43b8ea02f2ae204506c7038f2b7f169abd74db8d
guyo-halake <guyohalakeofficial@gmail.com>
2026-04-22 16:27:21 +0300

    razak: synced src/scratch/check_principal_school.cjs

---
+++ src/scratch/check_principal_school.cjs
@@ -0,0 +1,26 @@
+const https = require('https');
+
+const options = {
+  hostname: 'vomsaqkhtturzqfuwxsn.supabase.co',
+  port: 443,
+  path: '/rest/v1/profiles?select=full_name,school_id&role=eq.PRINCIPAL&limit=1',
+  method: 'GET',
+  headers: {
+    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbXNhcWtodHR1cnpxZnV3eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MDIxNjgsImV4cCI6MjA5MTI3ODE2OH0.3M2p0SWKO51yjkGtLnCyJOFsKiAwC__BnWoe_jZlPI8',
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbXNhcWtodHR1cnpxZnV3eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MDIxNjgsImV4cCI6MjA5MTI3ODE2OH0.3M2p0SWKO51yjkGtLnCyJOFsKiAwC__BnWoe_jZlPI8'
+  }
+};
+
+const req = https.request(options, (res) => {
+  let data = '';
+  res.on('data', (chunk) => { data += chunk; });
+  res.on('end', () => {
+    console.log('--- PRINCIPAL PROFILE ---');
+    console.log(data);
+  });
+});
+
+req.on('error', (e) => {
+  console.error(e);
+});
+req.end();
