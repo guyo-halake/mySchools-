ad09db4410430a7a9266c5828be7567f40a3591a
guyo-halake <guyohalakeofficial@gmail.com>
2026-04-22 16:27:10 +0300

    razak: synced src/admin/scratch/check_school_constraints.cjs

---
+++ src/admin/scratch/check_school_constraints.cjs
@@ -0,0 +1,22 @@
+const { Client } = require('pg');
+const connectionString = 'postgresql://postgres.vomsaqkhtturzqfuwxsn:Guyesa_10333@aws-0-eu-west-1.pooler.supabase.com:5432/postgres';
+const client = new Client({ connectionString });
+
+async function checkSchoolConstraints() {
+  try {
+    await client.connect();
+    const res = await client.query(`
+      SELECT conname, pg_get_constraintdef(c.oid) 
+      FROM pg_constraint c 
+      JOIN pg_namespace n ON n.oid = c.connamespace 
+      WHERE conrelid = 'schools'::regclass
+    `);
+    console.log('CONSTRAINTS:', JSON.stringify(res.rows));
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
+}
+
+checkSchoolConstraints();
