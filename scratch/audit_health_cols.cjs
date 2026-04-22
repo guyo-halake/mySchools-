4bd37470e9ce4d9cf7e2a742bc74708226b0d367
guyo-halake <guyohalakeofficial@gmail.com>
2026-04-22 16:26:54 +0300

    razak: synced scratch/audit_health_cols.cjs

---
+++ scratch/audit_health_cols.cjs
@@ -0,0 +1,18 @@
+const { Client } = require('pg');
+const connectionString = 'postgresql://postgres.vomsaqkhtturzqfuwxsn:Guyesa_10333@aws-0-eu-west-1.pooler.supabase.com:5432/postgres';
+
+async function audit() {
+  const client = new Client({ connectionString });
+  try {
+    await client.connect();
+    const res = await client.query("SELECT * FROM student_health LIMIT 1");
+    console.log(Object.keys(res.rows[0] || {}));
+    console.table(res.rows);
+  } catch (err) {
+    console.error(err);
+  } finally {
+    await client.end();
+  }
+}
+
+audit();
