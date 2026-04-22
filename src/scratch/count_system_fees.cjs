e38a57bac07de5eadb01602cbd8d72ad7ad64e3e
guyo-halake <guyohalakeofficial@gmail.com>
2026-04-22 16:27:23 +0300

    razak: synced src/scratch/count_system_fees.cjs

---
+++ src/scratch/count_system_fees.cjs
@@ -0,0 +1,29 @@
+const https = require('https');
+
+const options = {
+  hostname: 'vomsaqkhtturzqfuwxsn.supabase.co',
+  port: 443,
+  path: '/rest/v1/fees?select=count&type=eq.School system fees',
+  method: 'GET',
+  headers: {
+    'apikey': 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbXNhcWtodHR1cnpxZnV3eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MDIxNjgsImV4cCI6MjA5MTI3ODE2OH0.3M2p0SWKO51yjkGtLnCyJOFsKiAwC__BnWoe_jZlPI8',
+    'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZvbXNhcWtodHR1cnpxZnV3eHNuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3MDIxNjgsImV4cCI6MjA5MTI3ODE2OH0.3M2p0SWKO51yjkGtLnCyJOFsKiAwC__BnWoe_jZlPI8',
+    'Range': '0-0',
+    'Prefer': 'count=exact'
+  }
+};
+
+const req = https.request(options, (res) => {
+  let data = '';
+  console.log('--- FEE COUNT CHECK ---');
+  console.log('Status Header:', res.headers['content-range']);
+  res.on('data', (chunk) => { data += chunk; });
+  res.on('end', () => {
+    console.log('Data:', data);
+  });
+});
+
+req.on('error', (e) => {
+  console.error(e);
+});
+req.end();
